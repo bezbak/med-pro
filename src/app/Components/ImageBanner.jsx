6775fd128c15acb1b9f240ffe2fef2e56c88305d
Perizat Melisova <perizat.melisova.kk@gmail.com>
2024-09-02 22:15:09 +0600

    22

--- src/app/Components/ImageBanner.jsx
+++ src/app/Components/ImageBanner.jsx
@@ -1,16 +1,16 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 
-const ImageBanner = ({ altText }) => (
-  <div className="relative bg-cover bg-center rounded-3xl h-[350px] w-full my-8 overflow-hidden mt-[70px]">
-    <Image
-      src="/imgObsessed.png"
-      alt={altText}
-      layout="fill"
-      objectFit="cover"
-      className="rounded-3xl"
-      priority
-    />
-  </div>
-);
+// const ImageBanner = ({ altText }) => (
+//   <div className="relative bg-cover bg-center rounded-3xl h-[350px] w-full my-8 overflow-hidden mt-[70px]">
+//     <Image
+//       src="/imgObsessed.png"
+//       alt={altText}
+//       layout="fill"
+//       objectFit="cover"
+//       className="rounded-3xl"
+//       priority
+//     />
+//   </div>
+// );
 
-export default ImageBanner;
+// export default ImageBanner;
