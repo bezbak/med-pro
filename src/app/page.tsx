ceb9f8cb946f248863df46e703dba94c789d224d
Perizat Melisova <perizat.melisova.kk@gmail.com>
2024-09-02 22:26:12 +0600

    333

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import Testimonial from './Components/Testimonial';
-import VideoSection from './components/VideoSection';
-import Features from './components/Features';
-import ImageBanner from './components/ImageBanner';
+// import VideoSection from './components/VideoSection';
+// import Features from './components/Features';
+// import ImageBanner from './components/ImageBanner';
 import { featuresData } from '@/data/data';
 import Hero from '@/components/home/Hero';
 import OurMission from '@/components/home/OurMission';
@@ -23,18 +23,18 @@ export default function Home() {
                         {/* <Testimonial /> */}
                     </div>
                     <div className="w-full h-full flex justify-center">
-                        <VideoSection
+                        {/* <VideoSection
                             videoUrl="/woman.png"
                             description="Видеоотзыв от Светланы"
-                        />
+                        /> */}
                     </div>
                 </div>
-                <div className="mb-12">
+                {/* <div className="mb-12">
                     <Features features={featuresData} />
-                </div>
-                <div className="mt-12">
+                </div> */}
+                {/* <div className="mt-12">
                     <ImageBanner altText="Image description" />
-                </div>
+                </div> */}
             </div>
 
             <Doctors />
